4f29af6bd843053440c82ed4fb16c39f2d1b34fe
Quankhong-2511 <khongquan354@gmail.com>
2023-08-31 10:41:35 +0700

    Quan

---
+++ project/app/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register('nhanvien', views.NhanVienViewSet, basename='nhanvien')
+router.register('phongban', views.PhongBanViewSet, basename='phongban')
+
+
+
+
+urlpatterns = [
+    
+    path('api/', include(router.urls)),
+    
+    
+]
